aa0c14274eb9491e9dc3cae7ce56c043c2cc255d
ali4j <rashidy.ehsan@gmail.com>
2020-03-25 22:39:51 +0330

    adds new integration test.

--- ewallet-core-service/src/main/resources/data.sql
+++ ewallet-core-service/src/main/resources/data.sql
@@ -10,6 +10,9 @@ INSERT INTO CARD (pan, name, expiration_date, balance, wallet_id) VALUES
 INSERT INTO CARD (pan, name, expiration_date, balance, wallet_id) VALUES
   ('0000000000000001', 'Monkey D. Luffy', '2020/11', 0, '708835db-5e8d-49df-b57a-60b912aca11b');
 
+INSERT INTO CARD (pan, name, expiration_date, balance, wallet_id) VALUES
+  ('0000000000000002', 'Monkey D. Luffy', '2020/10', 0, '708835db-5e8d-49df-b57a-60b912aca11b');
+
 INSERT INTO wallet_owner (email, full_name, wallet_id) VALUES
   ('test@test.com', 'Monkey D. Luffy', '708835db-5e8d-49df-b57a-60b912aca11b');
 
